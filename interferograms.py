a6e43c59c99cd2d339b5cab9753eb46dd172f345
Zach Hoppinen <zachhoppinen@gmail.com>
2024-08-05 21:59:24 +0000

    intefereogram

--- interferograms.py
+++ interferograms.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
+import pandas as pd
+
 import py_gamma as pg
 
 from gs_funcs import get_width
@@ -14,21 +17,31 @@ dem_width=get_width(dem_par)
 
 main_dir = Path('/data/wcp/')
 SLCs = main_dir.rglob('*.slc')
-print(list(SLCs))
+# print(sorted(list(SLCs)))
 
-# # calculate VV interferogram, 7x7 rectangular averaging window, no decimation
-# slc1_VV='20230802/processed/PF_100MHz_H_6_20230802_220138_H_R1_1_HH.slc'
-# slc2_VV='20230802/processed/PF_100MHz_H_6_20230802_205833_H_R1_1_HH.slc'
-# intf_VV='20230802/spatialbaseline_ints/HH_20230802_205833_220138.int'
-# mli1_VV='20230802/spatialbaseline_ints/HH_20230802_205833_220138.mli'
-# mli2_VV='20230802/spatialbaseline_ints/HH_20230802_205833_220138.mli'
-# cc_VV='20230802/spatialbaseline_ints/HH_20230802_205833_220138.cc'
-# DEM_par2='20230802/spatialbaseline_ints/HH_20230802_205833_220138.dem_par'
+from itertools import combinations
 
-# SLC_intf_geo2 $slc1_VV $slc2_VV $DEM_par $intf_VV $mli1_VV $mli2_VV $cc_VV $DEM_par2 1 1 7 7 0
-# rasdt_pwr $cc_VV $shd $demWidth 1 0 1 1 .1 1.0 0 cc.cm -
-# rasmph_pwr $intf_VV $shd $demWidth 1 0 1 1 rmg.cm - 1.5 1. 24
-  
-# mk_kml $DEM_par2 $intf_VV".bmp" $intf_VV".kml"
-# mk_kml $DEM_par2 $cc_VV".bmp" $cc_VV".kml"
+int_dir = main_dir.joinpath('ints')
+int_dir.mkdir(exist_ok = True)
 
+for slc1, slc2 in combinations(SLCs, 2):
+
+    d1, d2 = [pd.to_datetime(f"{f.stem.split('_')[4]}T{f.stem.split('_')[5]}") for f in [slc1, slc2]]
+    if d1 > d2: slc1, slc2 = slc2, slc1
+
+    d1, d2 = [pd.to_datetime(f"{f.stem.split('_')[4]}T{f.stem.split('_')[5]}") for f in [slc1, slc2]]
+    mli1, mli2 = slc1.with_suffix('.mli'), slc2.with_suffix('.mli')
+    
+    d1_d2_str = f"{d1.strftime('%Y-%m-%dT%H-%M')}_{d2.strftime('%Y-%m-%dT%H-%M')}"
+    int_fp = int_dir.joinpath(d1_d2_str).with_suffix('.int')
+    cc_fp = int_dir.joinpath(d1_d2_str).with_suffix('.cc')
+    int_par = int_dir.joinpath(d1_d2_str).with_suffix('.dem_par')
+
+    pg.SLC_intf_geo2(slc1, slc2, dem_par, int_fp, mli1, mli2, cc_fp, int_par, 1, 1, 7, 7, 0)
+
+    print(cc_fp.with_suffix('.cc.bmp'))
+    pg.rasdt_pwr(cc_fp, shade, dem_width, 1, 0, 1, 1, .1, 1.0, 0, 'cc.cm' , cc_fp.with_suffix('.cc.bmp'))
+    pg.rasmph_pwr(int_fp, shade, dem_width, 1, 0, 1, 1, 'rmg.cm', int_fp.with_suffix('.int.bmp'), 1.5, 1.0, 24)
+
+    pg.mk_kml(int_par, int_fp.with_suffix('.int.bmp'), int_fp.with_suffix('.int.kml'))
+    pg.mk_kml(int_par, cc_fp.with_suffix('.cc.bmp'), cc_fp.with_suffix('.cc.kml'))
